445a615bf028e96fcf9057383dbfbd11ed8f1e61
Dariusz Wasilewski <dariusz.wasilewski.86@gmail.com>
2023-05-13 16:22:57 +0200

    add some fix

--- src/Redux/tablesRedux.js
+++ src/Redux/tablesRedux.js
@@ -83,7 +83,7 @@ export const fetchTables = () => {
 export const moveRequest = (tableId, target) => {
   return (dispatch) => {
     httpClient.patch(`${API_URL}/tables/${tableId}`, { listId: target }).then(() => {
-      dispatch(storeAddTable(tableId));
+      dispatch(changeList(tableId, target));
     });
   };
 };
@@ -91,7 +91,7 @@ export const moveRequest = (tableId, target) => {
 export const addTableRequest = (tableData, target) => {
   return (dispatch) => {
     httpClient.post(`${API_URL}/tables`, { ...tableData, listId: target }).then(() => {
-      dispatch(storeAddTable(tableData));
+      dispatch(storeAddTable(tableData, target));
     });
   };
 };
